3de558333acc039196ceb98504a9d500f207956e
Narix <tech@ribinin.at>
2023-07-05 13:48:20 +0200

    Added "feedback button"

--- js/index.js
+++ js/index.js
@@ -25,3 +25,27 @@ window.addEventListener('DOMContentLoaded', (event) => {
                 banner.style.transform = 'scale(1)';
             });
         });
+
+        function changeShape(event) {
+            const shape = document.getElementById("shape");
+            const content = document.getElementById("content");
+            
+            if (event.target === shape) {
+              if (shape.classList.contains("clicked")) {
+                shape.classList.remove("clicked");
+                content.textContent = "";
+              } else {
+                shape.classList.add("clicked");
+                content.textContent = "Do you like my Website? Are there any improvements that need to be done? I would be happy to get feedback from you on Discord!";
+              }
+            }
+          }
+      
+          document.addEventListener("click", function(event) {
+            const shape = document.getElementById("shape");
+            
+            if (!shape.contains(event.target)) {
+              shape.classList.remove("clicked");
+              document.getElementById("content").textContent = "";
+            }
+          });
